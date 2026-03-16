f74a316827719cebd85eb29738511522faff6e68
kjetil <kjetil.sigvartsen@nhn.no>
2026-03-16 08:55:22 +0100

    chore: tested dynamic client against custom resources

--- cmd/viti-agent/main.go
+++ cmd/viti-agent/main.go
@@ -1,59 +1,42 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"os"
-	"path/filepath"
+	"context"
+	"log/slog"
+	"time"
 
+	"github.com/NorskHelsenett/ror-viti-agent/internal/clients/viticlient"
 	"github.com/NorskHelsenett/ror-viti-agent/internal/config"
-	"k8s.io/client-go/dynamic"
-	"k8s.io/client-go/tools/clientcmd"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func main() {
 
-	_, err := config.NewConfig()
+	conf, err := config.NewConfig()
 	if err != nil {
 		panic(err)
 	}
 
-	_, err = createk8sDynamicClient()
+	slog.SetLogLoggerLevel(slog.Level(conf.LogLevel))
+
+	dynamic, err := viticlient.CreateK8sDynamicClient()
 	if err != nil {
 		panic(err)
 	}
 
-	//vitiClient := viticlient.NewForConfi
-}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-func createk8sDynamicClient() (*dynamic.DynamicClient, error) {
-	var kubeconfig *string
-	if home := homeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"),
-			"(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	}
-	flag.Parse()
+	for {
+		resources, err := dynamic.Resource(*viticlient.NewGVR("stable.example.com", "v1", "crontabs")).List(ctx, metav1.ListOptions{})
+		if err != nil {
+			slog.ErrorContext(ctx, "failed to gather resource(s)", "error", err)
+			time.Sleep(time.Second * 5)
+			continue
+		}
 
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	if err != nil {
-		err := fmt.Errorf("could not build kubeconfig: %w\n", err)
-		return nil, err
+		slog.InfoContext(ctx, "found resources", "resource_count", len(resources.Items))
+		time.Sleep(time.Second * 5)
 	}
 
-	dClient, err := dynamic.NewForConfig(config)
-	if err != nil {
-		err := fmt.Errorf("could not create dynamic client: %w\n", err)
-		return nil, err
-	}
-
-	return dClient, nil
-}
-
-func homeDir() string {
-	if h := os.Getenv("HOME"); h != "" {
-		return h
-	}
-	return os.Getenv("USERPROFILE") // Windows
 }
