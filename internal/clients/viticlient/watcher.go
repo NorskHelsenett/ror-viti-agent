ea9dda68e658a2619b1f79981b489e789ecbc9aa
kjetil <kjetil.sigvartsen@nhn.no>
2026-03-18 08:42:48 +0100

    chore: watcher testing

--- internal/clients/viticlient/watcher.go
+++ internal/clients/viticlient/watcher.go
@@ -2,8 +2,13 @@ package viticlient
 
 import (
 	"context"
+	"time"
 
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/kubernetes"
 )
 
 type Event string
@@ -21,4 +26,31 @@ type MachineEvent struct {
 
 type Watcher interface {
 	Watch(ctx context.Context) <-chan MachineEvent
+}
+
+func (c *ClientWatcher) Watch1(ctx context.Context, client kubernetes.Interface, gvr schema.GroupVersionResource) (watch.Interface, error) {
+
+	watcher, err := c.dynamicClient.Resource(gvr).Watch(ctx, v1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	return watcher, nil
+}
+
+func (c *ClientWatcher) Watch2(ctx context.Context, client kubernetes.Interface, gvr schema.GroupVersionResource, synctime time.Duration) (<-chan MachineEvent, error) {
+
+	output := make(chan MachineEvent)
+	//
+	// factory := informers.NewSharedInformerFactory(client, synctime)
+	// informer, err := factory.ForResource(gvr)
+	// if err != nil {
+	// }
+	// watcher, err := c.dynamicClient.Resource(gvr).Watch(ctx, v1.ListOptions{})
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// resultChan := watcher.ResultChan()
+
+	return output, nil
 }
