cf1798a9c1b83adb8c4025e751df4c335b5474ee
kjetil <kjetil.sigvartsen@nhn.no>
2026-03-18 08:38:57 +0100

    handler testing

--- cmd/viti-agent/main.go
+++ cmd/viti-agent/main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
-	"time"
 
 	"github.com/NorskHelsenett/ror-viti-agent/internal/clients/viticlient"
 	"github.com/NorskHelsenett/ror-viti-agent/internal/config"
-	"github.com/NorskHelsenett/ror-viti-agent/internal/converter"
-	"github.com/goforj/godump"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"github.com/NorskHelsenett/ror-viti-agent/internal/rorclient"
+	"github.com/vitistack/common/pkg/v1alpha1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/dynamic/dynamicinformer"
+	"k8s.io/client-go/tools/cache"
 )
 
 func main() {
@@ -21,38 +24,114 @@ func main() {
 
 	slog.SetLogLoggerLevel(slog.Level(conf.LogLevel))
 
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	_, err = rorclient.NewRorClient(
+		ctx,
+		conf.RorApikey,
+		conf.RorUrl,
+		conf.RorRole,
+		conf.RorVersion,
+		conf.RorCommit,
+	)
+
 	dynamic, err := viticlient.CreateK8sDynamicClient()
 	if err != nil {
 		panic(err)
 	}
 
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	// static, err := viticlient.CreateK8sStaticClient(false)
 
-	for {
-		resources, err := dynamic.Resource(*viticlient.NewGVRV1Alpha1Machine()).List(ctx, metav1.ListOptions{})
-		if err != nil {
-			slog.ErrorContext(ctx, "failed to gather resource(s)", "error", err)
-			time.Sleep(time.Second * time.Duration(conf.PollInterval))
-			continue
-		}
+	factory := dynamicinformer.NewFilteredDynamicSharedInformerFactory(dynamic, 5, "", nil)
+	informer := factory.ForResource(*viticlient.NewGVRV1Alpha1Machine()).Informer()
 
-		slog.InfoContext(ctx, "found resources", "resource_count", len(resources.Items))
-		machines, err := viticlient.MarshalMachineObjects(resources.Items)
-		if err != nil {
-			slog.ErrorContext(ctx, "failed to convert from unstructured", "error", err)
-			time.Sleep(time.Second * time.Duration(conf.PollInterval))
-			continue
-		}
-		rormachines, err := converter.ConvertToRorMachines(machines)
-		if err != nil {
-			slog.ErrorContext(ctx, "failed to convert to ror resources", "error", err)
-			time.Sleep(time.Second * 5)
-			continue
-		}
-		godump.DumpJSON(rormachines)
+	informer.RunWithContext(ctx)
+	informer.AddEventHandler(
+		cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj any) {
+				var machine v1alpha1.Machine
+				err := runtime.DefaultUnstructuredConverter.FromUnstructured(obj.(*unstructured.Unstructured).Object, &machine)
+				if err != nil {
+					panic(fmt.Errorf("failed to cast to machine"))
+				}
+				slog.Info("added machine", "name", machine.Name)
+			},
+			UpdateFunc: func(oldObj, newObj any) {
+				oldMachine, ok := oldObj.(v1alpha1.Machine)
+				if !ok {
+					panic(fmt.Errorf("failed to cast to oldMachine"))
+				}
 
-		time.Sleep(time.Second * time.Duration(conf.PollInterval))
+				newMachine, ok := newObj.(v1alpha1.Machine)
+				if !ok {
+					panic(fmt.Errorf("failed to cast to newMachine"))
+				}
+				slog.Info("updated machine", "old_name", oldMachine.Name, "new_machine", newMachine.Name)
+
+			},
+			DeleteFunc: func(obj any) {
+				machine, ok := obj.(v1alpha1.Machine)
+				if !ok {
+					panic(fmt.Errorf("failed to cast to machine"))
+				}
+				slog.Info("deleted  machine", "name", machine.Name)
+
+			},
+		},
+	)
+
+	stopCh := make(chan struct{})
+	defer close(stopCh)
+	factory.Start(stopCh)
+
+	if !cache.WaitForCacheSync(stopCh, informer.HasSynced) {
+		panic("failed to sync cache")
 	}
+
+	slog.Info("cache synced, watching changes")
+	<-stopCh
+
+	// watcher, err := c.dynamicClient.Resource(gvr).Watch(ctx, v1.ListOptions{})
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// resultChan := watcher.ResultChan()
+	// watcher, err := dynamic.Resource(*viticlient.NewGVRV1Alpha1Machine()).Watch(ctx, v1.ListOptions{})
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// resultChan := watcher.ResultChan()
+	// for {
+	// 	result := <-resultChan
+	// 	godump.DumpJSON(result)
+	// }
+	// for {
+	// 	resources, err := dynamic.Resource(*viticlient.NewGVRV1Alpha1Machine()).List(ctx, metav1.ListOptions{})
+	// 	if err != nil {
+	// 		slog.ErrorContext(ctx, "failed to gather resource(s)", "error", err)
+	// 		time.Sleep(time.Second * time.Duration(conf.PollInterval))
+	// 		continue
+	// 	}
+	//
+	// 	slog.InfoContext(ctx, "found resources", "resource_count", len(resources.Items))
+	// 	machines, err := viticlient.MarshalMachineObjects(resources.Items)
+	// 	if err != nil {
+	// 		slog.ErrorContext(ctx, "failed to convert from unstructured", "error", err)
+	// 		time.Sleep(time.Second * time.Duration(conf.PollInterval))
+	// 		continue
+	// 	}
+	// 	rormachines, err := converter.ConvertToRorMachines(machines)
+	// 	if err != nil {
+	// 		slog.ErrorContext(ctx, "failed to convert to ror resources", "error", err)
+	// 		time.Sleep(time.Second * 5)
+	// 		continue
+	// 	}
+	//
+	// 	rclient.UpdateRorResources(rormachines)
+	// 	godump.DumpJSON(rormachines)
+	//
+	// 	time.Sleep(time.Second * time.Duration(conf.PollInterval))
+	// }
 
 }
