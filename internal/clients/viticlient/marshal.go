c107e9b46be14dc8b4e38c31cbf49b0b95969f7e
kjetil <kjetil.sigvartsen@nhn.no>
2026-03-18 08:40:45 +0100

    feature: added single machine marshaler

--- internal/clients/viticlient/marshal.go
+++ internal/clients/viticlient/marshal.go
@@ -1,21 +1,35 @@
 package viticlient
 
 import (
+	"errors"
+
 	"github.com/vitistack/common/pkg/v1alpha1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
+var (
+	ErrUnexpectedGroupVersionKind = errors.New("unexpected group version kind")
+)
+
+func MarshalMachineObject(unstructured *unstructured.Unstructured, machine *v1alpha1.Machine) error {
+	if unstructured.GroupVersionKind() != NewGVRV1Alpha1Machine().GroupVersion().WithKind("Machine") {
+		return ErrUnexpectedGroupVersionKind
+	}
+	err := runtime.DefaultUnstructuredConverter.FromUnstructured(unstructured.Object, machine)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 // MarshalMachineObjects takes a slice of unstructured.Unstructured and converts it to v1alpha1.Machine.
 // It checks each resource if it matches the expected GroupVersionKind with the Machine definition,
 // and converts any matches to the output.
-func MarshalMachineObjects(unstructured []unstructured.Unstructured) ([]*v1alpha1.Machine, error) {
+func MarshalMachineObjects(unstructured []*unstructured.Unstructured) ([]*v1alpha1.Machine, error) {
 	output := make([]*v1alpha1.Machine, len(unstructured))
 	for index, resource := range unstructured {
-		if resource.GroupVersionKind() != NewGVRV1Alpha1Machine().GroupVersion().WithKind("Machine") {
-			continue
-		}
-		err := runtime.DefaultUnstructuredConverter.FromUnstructured(resource.Object, &output[index])
+		err := MarshalMachineObject(resource, output[index])
 		if err != nil {
 			return nil, err
 		}
