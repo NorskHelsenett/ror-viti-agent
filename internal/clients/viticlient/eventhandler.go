ecbfa273c2e4929c6502922afd6f7a8cba5436d4
kjetil <kjetil.sigvartsen@nhn.no>
2026-03-18 12:26:49 +0100

    feature: added marshaling logic to handlers

--- internal/clients/viticlient/eventhandler.go
+++ internal/clients/viticlient/eventhandler.go
@@ -1,46 +1,48 @@
 package viticlient
 
 import (
-	"fmt"
 	"log/slog"
+	"reflect"
 
 	"github.com/vitistack/common/pkg/v1alpha1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
 func AddFunc(obj any) {
 
 	var machine v1alpha1.Machine
-	structured, ok := obj.(*unstructured.Unstructured)
-	if !ok {
-		slog.Error("failed to cast to unstructured.Unstructured")
-		return
-	}
-	err := MarshalMachineObject(structured, &machine)
+	err := MarshalAnyMachineObject(obj, &machine)
 	if err != nil {
-		slog.Error("failed to cast into machine")
+		slog.Error("failed to marshal", "error", err)
 		return
 	}
 	slog.Info("added machine", "name", machine.Name)
 }
 func UpdateFunc(oldObj, newObj any) {
-	oldMachine, ok := oldObj.(v1alpha1.Machine)
-	if !ok {
-		panic(fmt.Errorf("failed to cast to oldMachine"))
+	var oldMachine v1alpha1.Machine
+	err := MarshalAnyMachineObject(oldObj, &oldMachine)
+	if err != nil {
+		slog.Error("failed to marshal old machine", "error", err)
+		return
 	}
 
-	newMachine, ok := newObj.(v1alpha1.Machine)
-	if !ok {
-		panic(fmt.Errorf("failed to cast to newMachine"))
+	var newMachine v1alpha1.Machine
+	err = MarshalAnyMachineObject(newObj, &newMachine)
+	if err != nil {
+		slog.Error("failed to marshal old machine", "error", err)
+		return
 	}
-	slog.Info("updated machine", "old_name", oldMachine.Name, "new_machine", newMachine.Name)
+
+	equality := reflect.DeepEqual(newMachine, oldMachine)
+	slog.Info("updated machine", "old_name", oldMachine.Name, "new_machine", newMachine.Name, "equality", equality)
 
 }
 func DeleteFunc(obj any) {
-	machine, ok := obj.(v1alpha1.Machine)
-	if !ok {
-		panic(fmt.Errorf("failed to cast to machine"))
+	var machine v1alpha1.Machine
+	err := MarshalAnyMachineObject(obj, &machine)
+	if err != nil {
+		slog.Error("failed to marshal", "error", err)
+		return
 	}
-	slog.Info("deleted  machine", "name", machine.Name)
+	slog.Info("deleted machine", "name", machine.Name)
 
 }
