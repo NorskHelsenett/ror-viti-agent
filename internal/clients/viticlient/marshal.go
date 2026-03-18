76eb62205bff00d11bedb6d24fb82f4e66f8e9e6
kjetil <kjetil.sigvartsen@nhn.no>
2026-03-18 08:51:10 +0100

    feature: added any to v1alpha1.Machine converter

--- internal/clients/viticlient/marshal.go
+++ internal/clients/viticlient/marshal.go
@@ -12,6 +12,21 @@ var (
 	ErrUnexpectedGroupVersionKind = errors.New("unexpected group version kind")
 )
 
+// MarshalAnyMachineObject converts the object input to an unstructured.Unstructured then to a v1alpha1.Machine.
+func MarshalAnyMachineObject(object any, machine *v1alpha1.Machine) error {
+	structured, ok := object.(*unstructured.Unstructured)
+	if !ok {
+		return errors.New("failed to cast to unstructured.Unstructured")
+
+	}
+	err := MarshalMachineObject(structured, machine)
+	if err != nil {
+		return errors.New("failed to cast into machine")
+
+	}
+	return nil
+}
+
 // MarshalMachineObject converts unstructured.unstructured and converts it to a v1alpha1.Machine.
 // It checks the input if it matches the expected GroupVersionKind with the Machine definition,
 // and converts any matches to the output.
