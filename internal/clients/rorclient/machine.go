ed45100dfbe0d3c8c7832af32c8a715857fce41d
Matias Skjetne <matias.skjetne@nhn.no>
2026-03-23 14:33:01 +0100

    feat: add machine client
    The machine client just wraps the Ror client and has funcitons for
    updating and deleting machines in ROR

---
+++ internal/clients/rorclient/machine.go
@@ -0,0 +1,61 @@
+package rorclient
+
+import (
+	"context"
+
+	"github.com/NorskHelsenett/ror-viti-agent/internal/converter"
+	"github.com/NorskHelsenett/ror/pkg/rorresources"
+	"github.com/vitistack/common/pkg/v1alpha1"
+)
+
+type MachineClient struct {
+	client RorClient
+}
+
+// deletes the machine in ROR
+func (c *MachineClient) Delete(ctx context.Context, machine v1alpha1.Machine) error {
+
+	return nil
+}
+
+// updates the provider status with the status from the Machine object
+func (c *MachineClient) UpdateProviderStatus(ctx context.Context, machine v1alpha1.Machine) error {
+
+	resource, err := vitiMachineToResource(machine)
+	if err != nil {
+		return nil
+	}
+
+	slice := []*rorresources.Resource{resource}
+
+	err = c.client.UpdateRorResources(slice)
+	if err != nil {
+		return nil
+	}
+
+	return nil
+}
+
+func vitiMachineToResourceSet(machines []v1alpha1.Machine) (*rorresources.ResourceSet, error) {
+
+	set := rorresources.NewResourceSet()
+	for _, machine := range machines {
+		resource, err := vitiMachineToResource(machine)
+		if err != nil {
+			return nil, err
+		}
+
+		set.Add(resource)
+	}
+
+	return nil, nil
+}
+
+func vitiMachineToResource(machine v1alpha1.Machine) (*rorresources.Resource, error) {
+	resource, err := converter.ConvertToRorMachine(&machine)
+	if err != nil {
+		return nil, err
+	}
+
+	return resource, nil
+}
